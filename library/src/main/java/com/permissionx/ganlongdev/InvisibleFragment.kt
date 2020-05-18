bc60db304c9672596de4c46c92c2e6d043b22dcd
ganlong <676001590@qq.com>
2020-05-18 17:04:54 +0800

    完成permissionX库

---
+++ library/src/main/java/com/permissionx/ganlongdev/InvisibleFragment.kt
@@ -0,0 +1,41 @@
+package com.permissionx.ganlongdev
+
+import android.content.pm.PackageManager
+import androidx.fragment.app.Fragment
+
+/**
+ * <pre>
+ *     author : ganlong
+ *     time   : 2020/05/18
+ *     desc   :
+ *     version: 1.0
+ * </pre>
+ */
+typealias PermissionCallback = (Boolean, List<String>) -> Unit
+
+class InvisibleFragment : Fragment() {
+
+    private var callback: PermissionCallback? = null
+
+    fun requestNow(cb: PermissionCallback, vararg permissions: String) {
+        callback = cb
+        requestPermissions(permissions, 1)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == 1) {
+            val deniedList = ArrayList<String>()
+            for ((index, result) in grantResults.withIndex()) {
+                if (result != PackageManager.PERMISSION_GRANTED) {
+                    deniedList.add(permissions[index])
+                }
+            }
+            val allGranted = deniedList.isEmpty()
+            callback?.let { it(allGranted, deniedList) }
+        }
+    }
+}
---
+++ library/src/main/java/com/permissionx/ganlongdev/PermissionX.kt
@@ -0,0 +1,32 @@
+package com.permissionx.ganlongdev
+
+import androidx.fragment.app.FragmentActivity
+
+/**
+ * <pre>
+ *     author : ganlong
+ *     time   : 2020/05/18
+ *     desc   :
+ *     version: 1.0
+ * </pre>
+ */
+object PermissionX {
+    private const val TAG = "InvisibleFragment"
+
+    fun request(
+        activity: FragmentActivity,
+        vararg permissions: String,
+        callback: PermissionCallback
+    ) {
+        val fragmentManager = activity.supportFragmentManager
+        val existedFragment = fragmentManager.findFragmentByTag(TAG)
+        val fragment = if (existedFragment != null) {
+            existedFragment as InvisibleFragment
+        } else {
+            val invisibleFragment = InvisibleFragment()
+            fragmentManager.beginTransaction().add(invisibleFragment, TAG).commitNow()
+            invisibleFragment
+        }
+        fragment.requestNow(callback, *permissions)
+    }
+}
